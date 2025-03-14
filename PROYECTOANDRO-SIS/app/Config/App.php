ea481cac610a1e338d6f5326f1276e106efc9af0
Carlos Rosales <tustasgamer@gmail.com>
2025-03-14 11:03:47 -0500

    Actualizar la URL base en la configuración y modificar el estilo del fondo en la página de inicio de sesión. Eliminar archivo de configuración de entorno obsoleto.

--- PROYECTOANDRO-SIS/app/Config/App.php
+++ PROYECTOANDRO-SIS/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:/miapp_ci4/public';
+    public string $baseURL = 'http://localhost:/andro-sis/proyectoandro-sis/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
