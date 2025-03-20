79ef039b51f8fc6c780f2e7e2e7e1b52f81fa38d
Carlos Rosales <tustasgamer@gmail.com>
2025-03-20 14:39:32 -0500

    `Update App.php to change baseURL to 'http://localhost/ANDRO-SIS/PROYECTOANDRO/public/'`

--- PROYECTOANDRO-SIS/app/Config/App.php
+++ PROYECTOANDRO-SIS/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/andro-sis/proyectoandro-sis/public/';
+    public string $baseURL = 'http://localhost/ANDRO-SIS/PROYECTOANDRO/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
